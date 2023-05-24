3f9d3de2bf1c0335e06da5c705657167ab00c7f7
soyoung5425 <soyoung5425@naver.com>
2023-05-24 12:49:42 +0900

    230524

--- Uni-Run/Assets/01.Scripts/ScrollingObject.cs
+++ Uni-Run/Assets/01.Scripts/ScrollingObject.cs
@@ -4,12 +4,12 @@ public class ScrollingObject : MonoBehaviour
 {
     public float speed = 10f; // 이동 속도
 
-    private void Update()
+    void Update()
     {
-        //게임오버가 아니라면
+        // 게임오버가 아니라면
         if (!GameManager.instance.isGameover)
         {
-            //초당 speed의 속도로 왼쪽으로 평행이동
+            // 초당 speed의 속도로 왼쪽으로 평행이동
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
     }
